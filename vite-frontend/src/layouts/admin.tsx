c3fbf539ccc8446078c365d4f281f7137eff5d50
abai569 <abaibubaix@gmail.com>
2026-07-22 23:51:45 +0800

    更改隧道菜单的位置

--- vite-frontend/src/layouts/admin.tsx
+++ vite-frontend/src/layouts/admin.tsx
@@ -173,20 +173,6 @@ export default function AdminLayout({
       ),
     },
     {
-      path: "/tunnel",
-      label: "隧道",
-      icon: (
-        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-          <path
-            clipRule="evenodd"
-            d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
-            fillRule="evenodd"
-          />
-        </svg>
-      ),
-      adminOnly: true,
-    },
-    {
       path: "/node",
       label: "节点",
       icon: (
@@ -212,6 +198,20 @@ export default function AdminLayout({
           />
         </svg>
       ),
+    },
+    {
+      path: "/tunnel",
+      label: "隧道",
+      icon: (
+        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+          <path
+            clipRule="evenodd"
+            d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
+            fillRule="evenodd"
+          />
+        </svg>
+      ),
+      adminOnly: true,
     },
     {
       path: "/sdwan",
