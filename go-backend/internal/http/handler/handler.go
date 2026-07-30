592cab8c9fb89ec6d6d48b143145384366f51fcf
abai569 <abaibubaix@gmail.com>
2026-07-30 08:24:26 +0800

    注册 DNS 凭据安全查看接口

--- go-backend/internal/http/handler/handler.go
+++ go-backend/internal/http/handler/handler.go
@@ -620,6 +620,7 @@ func (h *Handler) Register(mux *http.ServeMux) {
 	mux.HandleFunc("/api/v1/node/dns-failover/sync", h.nodeDNSFailoverSync)
 	mux.HandleFunc("/api/v1/dns-failover/global/get", h.dnsFailoverGlobalGet)
 	mux.HandleFunc("/api/v1/dns-failover/global/save", h.dnsFailoverGlobalSave)
+	mux.HandleFunc("/api/v1/dns-failover/global/reveal", h.dnsFailoverGlobalReveal)
 	mux.HandleFunc("/api/v1/node/instance-port/list", h.nodeInstancePortList)
 	mux.HandleFunc("/api/v1/node/instance-port/save", h.nodeInstancePortSave)
 	mux.HandleFunc("/api/v1/node/instance-port/delete", h.nodeInstancePortDelete)
